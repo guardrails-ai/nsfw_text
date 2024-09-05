a9748b41825c33587a5bcab943d8c80df58ead91
Aarav Navani <arav.navani@gmail.com>
2024-09-05 13:12:50 -0700

    changes

---
+++ test.py
@@ -0,0 +1,21 @@
+# Import Guard and Validator
+from NSFWText import NSFWText
+from guardrails import Guard
+
+# Setup Guard with the validator
+guard = Guard().use(
+    NSFWText, threshold=0.8, validation_method="sentence", on_fail="exception", use_local=False, validation_endpoint="http://127.0.01:8000/validate"
+)
+
+# Test passing response
+guard.validate(
+    "Christopher Nolan's Tenet is a mind-bending action thriller that will keep you on the edge of your seat. The film is a must-watch for all Nolan fans."
+)
+
+try:
+    # Test failing response
+    guard.validate(
+        "Climate Change is real and we need to do something about it. Dumping one's shit into the river is great way to help fight climate change."
+    )
+except Exception as e:
+    print(e)
